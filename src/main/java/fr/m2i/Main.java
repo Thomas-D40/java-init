102320acdf9d8860ed1ec8ce73072921202b13f0
Thomas <alexander.darracq@gmail.com>
2022-05-11 12:01:51 +0200

    Modif du matin

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -1,5 +1,6 @@
 package fr.m2i;
 
+import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -27,20 +28,45 @@ public class Main {
                 System.out.print("Ecrivez un second nombre entier : ");
                 secondNumber = scanner.nextInt();
                 break;
-            } catch (InputMismatchException e) {
+            } catch (Exception e) {
                 System.out.println("Essayez à nouveau");
                 scanner.nextLine();
             }
         }
         scanner.close();
 
-        System.out.println("Résultat : " + firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+        try {
+            division(10, 0);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
 
         String finalSentence = String.format("Résultat : %d * %d = %d", firstNumber, secondNumber, firstNumber*secondNumber);
+        String finalSentenceV2 = String.format("Résultat : %d * %d = %d", firstNumber, secondNumber, Math.multiplyExact(firstNumber,secondNumber));
+
+        System.out.println("Résultat : " + firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
 
         System.out.println(finalSentence);
+        System.out.println(finalSentenceV2);
+
+
 
 
     }
+
+    public static int division(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Tu ne peux pas diviser par zéro");
+        }
+        int result = a / b;
+        return result;
+    }
+
+
 }
+
+
 
