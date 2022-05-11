9499ca8c80b4a89c9843cf957966cff922525d63
Thomas <alexander.darracq@gmail.com>
2022-05-11 15:16:27 +0200

    Optimisation solution

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -1,6 +1,7 @@
 package fr.m2i;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,23 +12,30 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> resTab = new ArrayList<>();
+
         while (true) {
             try {
                 System.out.print("Ecrivez un premier nombre entier : ");
                 firstNumber = scanner.nextInt();
+
+
+                String phrase = "%d X %d = %d";
+
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(String.format(phrase, firstNumber, i, firstNumber*i));
+                    resTab.add(firstNumber*i);
+                }
+
                 break;
+
             } catch (InputMismatchException e) {
                 System.out.println("Essayez à nouveau");
                 scanner.nextLine();
             }
-        }
-        scanner.close();
 
-        String phrase = "%d X %d = %d";
-
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(String.format(phrase, firstNumber, i, firstNumber*i));
         }
+        System.out.println(resTab);
 
 
     }
