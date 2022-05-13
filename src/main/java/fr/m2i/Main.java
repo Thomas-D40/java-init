98b5110d6c6b2ca2f436d8bf3826c28e321ea1e4
Thomas-D40 <alexander.darracq@gmail.com>
2022-05-13 11:49:09 +0200

    Test

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -6,35 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int firstNumber;
-        int secondNumber;
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            try {
-                System.out.print("Ecrivez un premier nombre entier : ");
-                firstNumber = scanner.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Essayez à nouveau");
-                scanner.nextLine();
-            }
-        }
-
-        while (true) {
-            try {
-                System.out.print("Ecrivez un second nombre entier : ");
-                secondNumber = scanner.nextInt();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Essayez à nouveau");
-                scanner.nextLine();
-            }
-        }
-
-        System.out.println("Résultat : " + firstNumber + " * " + secondNumber + " = "   + (firstNumber * secondNumber));
-
 
     }
 }
---
+++ src/main/java/fr/m2i/Vehicle.java
@@ -0,0 +1,63 @@
+package fr.m2i;
+
+public abstract class Vehicle {
+
+    // Attributs
+    protected float currentGas;
+
+    protected float gasCapacity;
+
+    protected float gasConsumption;
+
+    protected int speed;
+
+    protected int passengerCapacity;
+
+    // Getters
+
+    public float getCurrentGas() {
+        return currentGas;
+    }
+
+    public float getGasCapacity() {
+        return gasCapacity;
+    }
+
+    public float getGasConsumption() {
+        return gasConsumption;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    // Setters
+
+    public void setCurrentGas(float currentGas) {
+        this.currentGas = currentGas;
+    }
+
+
+    public void setGasCapacity(float gasCapacity) {
+        this.gasCapacity = gasCapacity;
+    }
+
+
+    public void setGasConsumption(float gasConsumption) {
+        this.gasConsumption = gasConsumption;
+    }
+
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+}
